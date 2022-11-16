ff6ad53f091e2ec5b1a6b0430f3aafa1e6a2a647
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2022-11-15 22:41:31 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
-
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
- return (<h1>Seja bem vindo</h1>)
+  return (
+    <>
+      <h1>Seja bem vindo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
