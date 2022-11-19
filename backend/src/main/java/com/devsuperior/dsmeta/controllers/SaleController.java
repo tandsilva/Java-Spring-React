c124a090170c83d8a31f91090f1c70aa148000f1
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2022-11-18 23:39:02 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,11 +3,14 @@ package com.devsuperior.dsmeta.controllers;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,7 +20,13 @@ public class SaleController {
 
         private SaleService service;
     @GetMapping//tenq ser extamente aqui
-        public List<Sale> findSales(){
-            return service.findSales();
+
+        public Page<Sale> findSales(
+                //filtro para trazer uma data max e min esta sendo colocado em texto porque na web os dados trafegan como texto
+              @RequestParam(value= "minDate",defaultValue = "") String minDate,//se nao ouver parametro sera considerado vazio
+              @RequestParam(value = "maxDate",defaultValue = "")  String maxDate,
+                Pageable pageable){
+
+        return service.findSales(minDate,maxDate,pageable );
         }
 }
